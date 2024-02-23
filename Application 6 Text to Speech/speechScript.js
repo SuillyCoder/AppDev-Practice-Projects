681e68be5d978f9f637d8cae70f4c703b7c911a3
Enzo Basuil <146545169+SuillyCoder@users.noreply.github.com>
2024-02-23 22:53:50 +0800

    Updated speechScript.js
    Added volume adjustment settings

--- Application 6 Text to Speech/speechScript.js
+++ Application 6 Text to Speech/speechScript.js
@@ -1,6 +1,9 @@
 let talkingVoice = new SpeechSynthesisUtterance();
 let voiceChange = [];
 let voiceSelector =  document.getElementById("voiceSelector");
+let voiceFile;
+var outputLevel = document.getElementById("volumeRange");
+let volume = Math.log(outputLevel.value / 100) / Math.log(100 / 100);
 
 window.speechSynthesis.onvoiceschanged = () => {
     voiceChange = window.speechSynthesis.getVoices();
@@ -14,6 +17,10 @@ voiceSelector.addEventListener("change", () => {
 })
 
 document.getElementById("button").addEventListener("click", () =>{
+    let volume = outputLevel.value / 100;
     talkingVoice.text =  document.getElementById("textarea").value;
-    window.speechSynthesis.speak(talkingVoice);
+    talkingVoice.volume = volume;
+    voiceFile = window.speechSynthesis.speak(talkingVoice);
+
 });
+
